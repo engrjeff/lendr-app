9e87cc5f88ea2563ddffcbc776a2e57598dd5357
engrjeff <engineerjeff15@gmail.com>
2024-09-24 16:19:52 +0800

    chore: remove unused files

--- app/(user)/dashboard/page.tsx
+++ app/(user)/dashboard/page.tsx
@@ -1,5 +1,16 @@
 function DashboardPage() {
-  return <div>DashboardPage</div>
+  return (
+    <div className="flex flex-col space-y-6">
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="text-xl font-bold">Dashboard</h1>
+          <p className="text-sm text-muted-foreground">
+            Quick insights on your balances.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default DashboardPage
--- components/features/units/types.ts
+++
@@ -1,5 +0,0 @@
-import { Unit, UnitConversion } from "@prisma/client"
-
-export type UnitWithConversion = Unit & {
-  conversions: UnitConversion[]
-}
