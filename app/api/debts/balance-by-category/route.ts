9cd7f300e078e4d0e6f53defa713e2695046e43e
engrjeff <engineerjeff15@gmail.com>
2024-10-12 06:57:33 +0800

    make GET dynamic

--- app/api/debts/balance-by-category/route.ts
+++ app/api/debts/balance-by-category/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server"
 import { getBalancesByCategory } from "@/queries/debt"
 
+export const dynamic = "force-dynamic"
+
 export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams
--- app/api/debts/debts-for-month/route.ts
+++ app/api/debts/debts-for-month/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server"
 import { getDebtsForMonth } from "@/queries/debt"
 
+export const dynamic = "force-dynamic"
+
 export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams
